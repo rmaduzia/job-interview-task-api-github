df528d685755c2af6aabb84e8127b6f7a73fba6d
rrmaduzia <rafi11m@hotmail.com>
2021-09-02 20:45:02 +0200

    initial commit

---
+++ src/main/java/com/jobinterview/jobinterviewtaskapigithub/JobInterviewTaskApiGithubApplication.java
@@ -0,0 +1,13 @@
+package com.jobinterview.jobinterviewtaskapigithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobInterviewTaskApiGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobInterviewTaskApiGithubApplication.class, args);
+    }
+
+}
